50f1eb6d4a132219f147a09bdb5edf7d17ff3a8e
karim <mail@mail.com>
2024-02-13 18:54:11 +0300

    commit2

---
+++ src/main/java/com/example/department/entity/Department.java
@@ -0,0 +1,26 @@
+package com.example.department.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "departments")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+
+
+}
